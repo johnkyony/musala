fdc48038717c3e5acba602c75930933a45ec1012
jkyony <jkyony@gmail.com>
2016-07-06 21:24:37 +0100

    fixed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,5 +15,6 @@ end
 Job.delete_all
 CSV.foreach(Rails.root.join("db/seeds_data/Job_database.csv"), headers: true) do |row|
   company = Company.find_by name: row[2]
- Job.find_or_create_by( description: row[0] , link: row[1],  company_id: company.id , place: row[3], posted: Date.strptime(row[4], '%Y-%m-%d') )
+
+ Job.find_or_create_by( description: row[0] , link: row[1],  company_id: company.id , place: row[3], posted: Date.parse(row[4]) )
 end
